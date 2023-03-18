0f001e245eb78d4be3e3f7657bec5f16e29665f9
blanusa <vlada2002blanusa@gmail.com>
2023-03-18 22:40:11 +0100

    Just added paths for database

--- Trippin Travel Agency/InitialProject/InitialProject/Context/DataBaseContext.cs
+++ Trippin Travel Agency/InitialProject/InitialProject/Context/DataBaseContext.cs
@@ -21,14 +21,19 @@ namespace InitialProject.Context
         public DbSet<GuestRate> GuestRate { get; set; }
 
         // Apsolute paths for every memeber of the team
-
+        // napisite cije je sta majmuni
         //public string path = @"C:\Users\ifeel\Documents\GitHub\Trippin-Travel-Agency-SIMS-Project\Trippin Travel Agency\InitialProject\InitialProject\MyDatabase.sqlite";
         // public string path = @"C:\Users\Nemanja\Desktop\Faks\Semestar 6\Projekat c#\Trippin-Travel-Agency-SIMS-Project\Trippin Travel Agency\InitialProject\InitialProject\MyDatabase.sqlite";
         // public string path = @"C:\Users\Nemanja\Documents\GitHub\SIMS-HCI-Project\Trippin Travel Agency\InitialProject\InitialProject\MyDatabase.sqlite";
+
         //Nemanja Ranitovic putanja desktop
         //public string path= @"C:\Users\Nemanja\Desktop\Faks\Semestar 6\Projekat c#\Trippin-Travel-Agency-SIMS-Project\Trippin Travel Agency\InitialProject\InitialProject\MyDatabase.sqlite";
+
         // blanusa lap
-        public string path = @"C:\Users\abc\Documents\GitHub\SIMS-HCI-Project\Trippin Travel Agency\InitialProject\InitialProject\MyDatabase.sqlite";
+        //public string path = @"C:\Users\abc\Documents\GitHub\SIMS-HCI-Project\Trippin Travel Agency\InitialProject\InitialProject\MyDatabase.sqlite";
+
+        //blanusa pc
+        public string path = @"C:\Users\vlada\GitHub\Trippin-Travel-Agency-SIMS-Project\Trippin Travel Agency\InitialProject\InitialProject\MyDatabase.sqlite"; 
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
