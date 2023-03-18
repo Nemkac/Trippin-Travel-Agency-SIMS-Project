8c793216b74dcb8e006a6b29f64c5248372d001e
Aleksa Simeunovic <84989791+aleksasimeunovic@users.noreply.github.com>
2023-03-18 19:25:53 +0100

    Added one more .xaml

--- Trippin Travel Agency/InitialProject/InitialProject/View/TrackTourLiveInterface.xaml.cs
+++ Trippin Travel Agency/InitialProject/InitialProject/View/TrackTourLiveInterface.xaml.cs
@@ -1,16 +1,13 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
-using System.Windows.Media; 
+using System.Windows.Media;
 
 namespace InitialProject.View
 {
-    /// <summary>
-    /// Interaction logic for TourGuideInterface.xaml
-    /// </summary>
-    public partial class TourGuideInterface : Window
+    public partial class TrackTourLiveInterface : Window
     {
-        public TourGuideInterface()
+        public TrackTourLiveInterface()
         {
             InitializeComponent();
         }
