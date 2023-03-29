3aa5798204b4a6b06d411e91b049a3fb066e071e
Nemkac <nemanjatodorovic132002002@gmail.com>
2023-03-29 18:04:41 +0200

    Created BookingRequest table

---
+++ Trippin Travel Agency/InitialProject/InitialProject/Migrations/20230329160314_addeTableBookingDelaymentRequest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InitialProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class addeTableBookingDelaymentRequest : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BookingDelaymentRequests",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    bookingId = table.Column<int>(type: "INTEGER", nullable: false),
+                    newArrival = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    newDeparture = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    status = table.Column<int>(type: "INTEGER", nullable: false),
+                    comment = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BookingDelaymentRequests", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BookingDelaymentRequests");
+        }
+    }
+}
---
+++ Trippin Travel Agency/InitialProject/InitialProject/Model/BookingDelaymentRequest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InitialProject.Model
+{
+
+    public enum Status
+    {
+        Pending,
+        Denied,
+        Accepted
+    }
+    public class BookingDelaymentRequest
+    {
+        public int id { get; set; }
+        public int bookingId { get; set; }
+        public DateTime newArrival { get; set; }
+        public DateTime newDeparture { get; set; }
+        public Status status { get; set; }
+        public string comment { get; set; }
+
+        public BookingDelaymentRequest() { }
+
+        public BookingDelaymentRequest(int bookingId, DateTime newArrival, DateTime newDeparture, Status status, string comment)
+        {
+            this.bookingId = bookingId;
+            this.newArrival = newArrival;
+            this.newDeparture = newDeparture;
+            this.status = status;
+            this.comment = comment;
+        }
+    }
+}
