e17c44b74ec2ba8b52524155419f135fa5b8d2bc
Aleksa Simeunovic <84989791+aleksasimeunovic@users.noreply.github.com>
2023-04-22 00:17:55 +0200

    Refreshing DataGrid as you type

---
+++ Trippin Travel Agency/InitialProject/InitialProject/Service/TourServices/TourRequestService.cs
@@ -0,0 +1,34 @@
+﻿using InitialProject.Context;
+using InitialProject.Model;
+using InitialProject.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using InitialProject.Interfaces;
+using InitialProject.Repository;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+
+namespace InitialProject.Service.TourServices
+{
+    class TourRequestService
+    {
+        private readonly ITourRepository iTourRepository;
+        public TourRequestService(ITourRepository iTourRepository)
+        {
+            this.iTourRepository = iTourRepository;
+        }
+
+        public List<TourRequest> GetAllFullTourRequests()
+        {
+            List<TourRequest> requests = new List<TourRequest>();
+            DataBaseContext context = new DataBaseContext();
+            foreach(TourRequest tr in context.TourRequests.ToList())
+            {
+                requests.Add(tr);
+            }
+            return requests;
+        }
+
+    }
+}
--- Trippin Travel Agency/InitialProject/InitialProject/WPF/View/TourGuideViews/TourGuide_FullTourRequests.xaml.cs
+++ Trippin Travel Agency/InitialProject/InitialProject/WPF/View/TourGuideViews/TourGuide_FullTourRequests.xaml.cs
@@ -24,12 +24,67 @@ namespace InitialProject.WPF.View.TourGuideViews
     /// </summary>
     public partial class TourGuide_FullTourRequests : UserControl
     {
-        private TourService tourService;
+        private TourRequestService tourRequestService;
         public TourGuide_FullTourRequests()
         {
             InitializeComponent();
-            this.tourService = new(new TourRepository());
+            this.tourRequestService = new(new TourRepository());
+            List<TourRequest> requests = tourRequestService.GetAllFullTourRequests();
+            tourRequestsDataGrid.ItemsSource = requests;
+        }
+        private void FilterData()
+        {
+            string country = tourCountryTextBox.Text;
+            string city = tourCityTextBox.Text;
+            string numOfPeople = tourRequestCountryComboBox.Text;
+            DateTime? startDate = startingDateDatePicker.SelectedDate;
+            DateTime? endDate = endingDateDatePicker.SelectedDate;
+            string language = tourRequestCityComboBox.Text;
+
+            IEnumerable<TourRequest> filteredRequests = tourRequestService.GetAllFullTourRequests();
+
+            if (!string.IsNullOrWhiteSpace(country))
+            {
+                filteredRequests = filteredRequests.Where(r => r.country.StartsWith(country, StringComparison.InvariantCultureIgnoreCase));
+            }
+
+            if (!string.IsNullOrWhiteSpace(city))
+            {
+                filteredRequests = filteredRequests.Where(r => r.city.StartsWith(city, StringComparison.InvariantCultureIgnoreCase));
+            }
+
+
+            if (!string.IsNullOrWhiteSpace(numOfPeople))
+            {
+                filteredRequests = filteredRequests.Where(r => r.numberOfTourists.ToString().StartsWith(numOfPeople));
+            }
+
+            if (!string.IsNullOrWhiteSpace(language))
+            {
+                filteredRequests = filteredRequests.Where(r => r.language.ToString().StartsWith(language, StringComparison.InvariantCultureIgnoreCase));
+            }
+
+            if (startDate.HasValue)
+                filteredRequests = filteredRequests.Where(r => r.startDate >= startDate.Value);
+
+            if (endDate.HasValue)
+                filteredRequests = filteredRequests.Where(r => r.endDate <= endDate.Value);
+
+            tourRequestsDataGrid.ItemsSource = filteredRequests.ToList();
         }
 
+
+
+
+
+        private void FilterParametersChanged(object sender, EventArgs e)
+        {
+            FilterData();
+        }
     }
+    
+
+    
+
+
 }
